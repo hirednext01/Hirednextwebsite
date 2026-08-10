1aa3be387881d8a9a2a3a4ecc1e5fbbd868c922a
hirednext01 <tarushikha@hirednext.info>
2026-08-11 01:18:24 +0530

    Route testimonials through reputation authority controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -44,7 +44,7 @@ $routes->get('contact', 'Home::contact');
 $routes->get('jobs', 'Jobs::index');
 $routes->get('jobs/(:any)', 'Home::jobDetail/$1');
 $routes->post('jobs/(:any)/apply', 'Home::applyJob/$1');
-$routes->get('testimonials', 'Authority::testimonials');
+$routes->get('testimonials', 'ReputationAuthority::testimonials');
 $routes->post('contact/submit', 'Home::submitContact');
 $routes->get('test', 'Test::index');
 
