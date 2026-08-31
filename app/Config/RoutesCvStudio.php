344d70b82e058d622b692681726cda5c630a3d97
hirednext01 <tarushikha@hirednext.info>
2026-08-31 15:08:49 +0530

    Add CV Studio admin routes

---
+++ app/Config/RoutesCvStudio.php
@@ -0,0 +1,11 @@
+<?php
+
+/** @var \CodeIgniter\Router\RouteCollection $routes */
+
+$routes->get('admin/cv-studio', 'CvStudioAdmin::index');
+$routes->get('admin/cv-studio/(:num)', 'CvStudioAdmin::detail/$1');
+$routes->post('admin/cv-studio/(:num)/generate', 'CvStudioAdmin::generate/$1');
+$routes->get('admin/cv-studio/(:num)/documents/(:num)', 'CvStudioAdmin::preview/$1/$2');
+$routes->get('admin/cv-studio/(:num)/documents/(:num)/word', 'CvStudioAdmin::downloadWord/$1/$2');
+$routes->post('admin/cv-studio/(:num)/documents/(:num)/deliver', 'CvStudioAdmin::deliver/$1/$2');
+$routes->post('admin/cv-studio/(:num)/documents/(:num)/branding', 'CvStudioAdmin::branding/$1/$2');
