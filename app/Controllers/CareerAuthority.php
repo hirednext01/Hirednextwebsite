f6f23ddece4342400d9bbbf6efd19a2e8cd6b876
hirednext01 <tarushikha@hirednext.info>
2026-09-21 20:49:32 +0530

    Fix: pass Career Intelligence article metadata to layout

--- app/Controllers/CareerAuthority.php
+++ app/Controllers/CareerAuthority.php
@@ -142,6 +142,12 @@ class CareerAuthority extends BaseController
             'related' => array_slice($related, 0, 4),
             'updatedOn' => $config->updatedOn,
             'reviewedBy' => $config->reviewedBy,
+            'ogType' => 'article',
+            'articleAuthor' => 'Taru Shikha',
+            'publishedTime' => $config->updatedOn,
+            'modifiedTime' => $config->updatedOn,
+            'articleSection' => 'Career Intelligence',
+            'articleTags' => ['CV evidence', 'LinkedIn positioning', 'Leadership careers', 'Recruiter visibility'],
             'jsonLd' => json_encode($jsonLd, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT),
         ]);
     }
